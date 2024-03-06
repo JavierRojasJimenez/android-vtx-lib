470ed85fe7693909ff1652e8ad09a756be3963cb
Javier Rojas <javier.rojas@cetrica.com>
2024-03-06 13:33:09 -0600

    [UPDATE] add Intent catalog

--- vertex-lib/src/main/java/com/example/vertex_lib/ButtonTest.kt
+++ vertex-lib/src/main/java/com/example/vertex_lib/ButtonTest.kt
@@ -1,16 +1,12 @@
 package com.example.vertex_lib
 
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.android.volley.Request
-import com.android.volley.Response
 
 
 class ButtonTest @JvmOverloads constructor(
@@ -22,26 +18,13 @@ class ButtonTest @JvmOverloads constructor(
     init {
         LayoutInflater.from(context).inflate(R.layout.component_btn1, this, true)
         val btn = findViewById<Button>(R.id.btn1)
-        //val txtRequest = findViewById<TextView>(R.id.text_request)
         var message1: String = "Mensaje de prueba desde libreria"
-        // var message2: String = "Hola Mundo"
 
-        //val requestQueue = Volley.newRequestQueue(context)
-        //val url = "http://www.google.com"
+        btn.setOnClickListener {
+            Toast.makeText(context, message1, Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, catalog::class.java)
+            context.startActivity(intent)
+        }
 
-        //val stringRequest = StringRequest(Request.Method.GET, url,
-        //    Response.Listener<String> { response ->
-        //        message2 = "r: ${response}"
-        //    },
-        //    Response.ErrorListener { error ->
-        //        message2 = "r: ${error}"
-        //    })
-
-
-        //requestQueue.add(stringRequest)
-
-
-        btn.setOnClickListener { Toast.makeText(context, message1, Toast.LENGTH_SHORT).show() }
-        //txtRequest.text = message2
     }
 }
---
+++ vertex-lib/src/main/java/com/example/vertex_lib/catalog.kt
@@ -0,0 +1,13 @@
+package com.example.vertex_lib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class catalog : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalog)
+    }
+}
